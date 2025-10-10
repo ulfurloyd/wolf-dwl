5cedb6759799ea9e873babe647159639c724aa01
ulfurloyd <eklavyasood@gmail.com>
2025-10-10 14:29:55 +0530

    feat: someblocks changes and scripts

--- someblocks/blocks.h
+++ someblocks/blocks.h
@@ -1,15 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{" :", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
-	
+	{"", "date '+%b %d (%a) %I:%M%p' ",					5,		0},
+
+	{"", "~/Dev/dwl/someblocks/scripts/volume", 0, 2},
+
+	{"", "~/Dev/dwl/someblocks/scripts/weather Jaipur+India", 0, 60},
 	/* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
 	/* {"", "date '+%b %d (%a) %I:%M%p'",					0,		10}, */
 };
-
-
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
